a5d942ea6af94e553ee06ec3ead30a530268aa9c
zhuzichu <zhuzichu520@gmail.com>
2019-11-01 20:56:33 +0800

    update

--- app/src/main/java/com/zhuzichu/android/libs/tool/ToolCommon.kt
+++ app/src/main/java/com/zhuzichu/android/libs/tool/ToolCommon.kt
@@ -7,14 +7,14 @@ package com.zhuzichu.android.libs.tool
  * 类型强转
  *
  */
-fun <T> cast(obj: Any?): T? {
-    return if (obj == null) null else obj as T
+fun <T> cast(obj: Any): T {
+    return obj as T
 }
 
 /**
  * 类型强转
  *
  */
-fun <T> Any?.toCast(): T? {
-    return if (this == null) null else this as T
+fun <T> Any.toCast(): T {
+    return this as T
 }
